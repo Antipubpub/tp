ec4ee1191917c232b5e35ad24c1b607c24701c68
ereith <ereith@invoke.fr>
2016-04-18 11:35:56 +0200

    premier commit

---
+++ tp/src/main/java/org/urouen/tp/Config/AppConfig.java
@@ -0,0 +1,11 @@
+package org.urouen.tp.Config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"org.urouen.tp.controller"})
+public class AppConfig {
+}
---
+++ tp/src/main/java/org/urouen/tp/controller/HomeController.java
@@ -0,0 +1,27 @@
+package org.urouen.tp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	private static final String HELLO_WORLD_MSG = "hello world!";
+	
+	@RequestMapping(value="/about", method=RequestMethod.GET)
+	@ResponseBody
+	public String about() {
+		return RequestMethod.GET.name() + " " + HELLO_WORLD_MSG;
+	}
+	
+	@RequestMapping(value="/about", method=RequestMethod.POST)
+	@ResponseBody
+	public String aboutPost() {
+		return RequestMethod.POST.name() + " " + HELLO_WORLD_MSG;
+	}
+	
+	
+
+}
